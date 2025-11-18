36467e404e1794d955fdcad53b60c308018e4aa6
Ronald Mannak <ronaldmannak@me.com>
2025-11-18 21:31:50 +0100

    show new chat button when sidebar is closed 5849f

--- tools/server/webui/src/lib/components/app/chat/ChatScreen/ChatScreenHeader.svelte
+++ tools/server/webui/src/lib/components/app/chat/ChatScreen/ChatScreenHeader.svelte
@@ -3,7 +3,7 @@
 	import { ChatSettingsDialog } from '$lib/components/app';
 	import { Button } from '$lib/components/ui/button';
 	import { useSidebar } from '$lib/components/ui/sidebar';
-
+	import { SquarePen } from '@lucide/svelte';
 
 	let settingsOpen = $state(false);
 	const sidebar = useSidebar();
@@ -14,12 +14,21 @@
 </script>
 
 <header
-	class={`md:background-transparent pointer-events-none fixed top-0 right-0 left-0 z-50 flex items-center justify-end bg-background/40 p-4 backdrop-blur-xl ${
+	class={`md:background-transparent pointer-events-none fixed top-0 right-0 left-0 z-50 flex items-center justify-between bg-background/40 p-4 backdrop-blur-xl ${
 		!sidebar.isMobile && sidebar.state === 'expanded'
 			? 'md:left-[var(--sidebar-width)]'
 			: 'md:left-0'
 	}`}
 >
+
+	<div class="pointer-events-auto ml-12 flex items-center space-x-2">
+		{#if (!sidebar.isMobile && sidebar.state === 'collapsed') || (sidebar.isMobile && !sidebar.openMobile)}
+			<Button variant="ghost" size="sm" href="?new_chat=true#/"
+				><SquarePen class="h-4 w-4" /></Button
+			>
+		{/if}
+	</div>
+
 	<div class="pointer-events-auto flex items-center space-x-2">
 		<Button variant="ghost" size="sm" onclick={toggleSettings}>
 			<Settings class="h-4 w-4" />
